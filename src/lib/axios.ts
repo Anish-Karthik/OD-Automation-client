618dd37450cb1ed3bc01cdc44c69dd3a2e6e7d3e
Anish-Karthik <anishkarthik.54321@gmail.com>
2024-10-26 19:21:42 +0530

    feat: added increment semester

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: " http://localhost:3000/trpc",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/trpc`,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
 export const auth = axios.create({
-  baseURL: " http://localhost:3000/api/auth",
+  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/auth`,
   headers: {
     "Content-Type": "application/json",
   },
